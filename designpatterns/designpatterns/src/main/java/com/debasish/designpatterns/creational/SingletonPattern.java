f681126ae1cf90b7e975df7d370df81ce8e58d32
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-03-25 08:01:51 +0530

    Added few update

--- designpatterns/designpatterns/src/main/java/com/debasish/designpatterns/creational/SingletonPattern.java
+++ designpatterns/designpatterns/src/main/java/com/debasish/designpatterns/creational/SingletonPattern.java
@@ -4,12 +4,10 @@ package com.debasish.designpatterns.creational;
 class SingletonExample {
     // Step 1: create a static variable of same class
     static SingletonExample instance;
-
     // Step 2: make constructor private so no one can create object using new
     private SingletonExample() {
         System.out.println("Constructor called");
     }
-
     // Step 3: provide a static method to get the singleton instance
     public static SingletonExample getInstance() {
         if (instance == null) {
@@ -21,16 +19,11 @@ class SingletonExample {
 
 // Main class to demonstrate Singleton Pattern
 public class SingletonPattern {
-
     public static void main(String[] args) {
-
         // Step 3: create object only if it is null (using getInstance method)
         SingletonExample obj1 = SingletonExample.getInstance(); // object created first time
-
         SingletonExample obj2 = SingletonExample.getInstance(); // using same object (no new creation)
-
         SingletonExample obj3 = SingletonExample.getInstance(); // using same object
-
         // check all references pointing to same object
         System.out.println("obj1 == obj2: " + (obj1 == obj2)); // true means same object
         System.out.println("obj2 == obj3: " + (obj2 == obj3)); // true means same object
