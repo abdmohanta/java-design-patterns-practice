e3cb4dd053f8c9f20aceddab4a0b10d2d73db37c
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-03-25 19:37:41 +0530

    Comment removed

--- designpatterns/designpatterns/src/main/java/com/debasish/designpatterns/creational/BuilderPattern.java
+++ designpatterns/designpatterns/src/main/java/com/debasish/designpatterns/creational/BuilderPattern.java
@@ -6,13 +6,11 @@ class House {
     private String foundation;
     private String walls;
     private String roof;
-
     // optional parameters
     private String windows;
     private String doors;
     private String garage;
     private String garden;
-
     // Constructor - private, only Builder can create it
     private House(HouseBuilder builder) {
         this.foundation = builder.foundation;
@@ -43,13 +41,11 @@ class House {
         private String foundation;
         private String walls;
         private String roof;
-
         // optional parameters
         private String windows;
         private String doors;
         private String garage;
         private String garden;
-
         // Constructor with required parameters
         public HouseBuilder(String foundation, String walls, String roof) {
             this.foundation = foundation;
@@ -62,22 +58,18 @@ class House {
             this.windows = windows;
             return this; // return this for method chaining
         }
-
         public HouseBuilder doors(String doors) {
             this.doors = doors;
             return this;
         }
-
         public HouseBuilder garage(String garage) {
             this.garage = garage;
             return this;
         }
-
         public HouseBuilder garden(String garden) {
             this.garden = garden;
             return this;
         }
-
         // build() method returns the constructed House object
         public House build() {
             return new House(this);
@@ -87,9 +79,7 @@ class House {
 
 // Main class to demonstrate Builder Pattern
 public class BuilderPattern {
-
     public static void main(String[] args) {
-
         // Create House using Builder pattern
         House house1 = new House.HouseBuilder("Concrete", "Bricks", "Tiles")
                 .windows("Wooden Windows")
@@ -97,26 +87,21 @@ public class BuilderPattern {
                 .garage("2-car Garage")
                 .garden("Rose Garden")
                 .build();
-
         System.out.println("House 1:");
         System.out.println(house1);
         System.out.println("\n");
-
         // Create another House with different optional parameters
         House house2 = new House.HouseBuilder("Stone", "Stone Blocks", "Slate")
                 .windows("Glass Windows")
                 .doors("Wooden Doors")
                 .build(); // no garage or garden
-
         System.out.println("House 2:");
         System.out.println(house2);
         System.out.println("\n");
-
         // Create third House with minimal parameters
         House house3 = new House.HouseBuilder("Wood", "Wood Panels", "Shingles")
                 .garage("1-car Garage")
                 .build();
-
         System.out.println("House 3:");
         System.out.println(house3);
     }
