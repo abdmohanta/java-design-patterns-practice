ed2a90b2e200adbba4bc45b728b269d517fdc232
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-03-28 22:17:09 +0530

    Commented code

--- designpatterns/designpatterns/src/main/java/com/debasish/designpatterns/behavioral/StatePattern.java
+++ designpatterns/designpatterns/src/main/java/com/debasish/designpatterns/behavioral/StatePattern.java
@@ -1,43 +1,33 @@
 package com.debasish.designpatterns.behavioral;
 
 public class StatePattern {
-
     public static void main(String[] args) {
-
         // Step 1: Create State interface
         interface State {
             void handle(); // behavior method
         }
-
         // Step 2: Create different states
-
         class RedState implements State {
             public void handle() {
                 System.out.println("RED Light -> Stop");
             }
         }
-
         class GreenState implements State {
             public void handle() {
                 System.out.println("GREEN Light -> Go");
             }
         }
-
         class YellowState implements State {
             public void handle() {
                 System.out.println("YELLOW Light -> Wait");
             }
         }
-
         // Step 3: Context class (Traffic Signal)
         class TrafficSignal {
-
             State state; // current state
-
             void setState(State state) {
                 this.state = state; // change state
             }
-
             void showSignal() {
                 state.handle(); // call behavior based on state
             }
@@ -46,13 +36,10 @@ public class StatePattern {
         // Step 4: Use the State Pattern
 
         TrafficSignal signal = new TrafficSignal(); // create context
-
         signal.setState(new RedState()); // set RED
         signal.showSignal(); // Stop
-
         signal.setState(new GreenState()); // change to GREEN
         signal.showSignal(); // Go
-
         signal.setState(new YellowState()); // change to YELLOW
         signal.showSignal(); // Wait
     }
