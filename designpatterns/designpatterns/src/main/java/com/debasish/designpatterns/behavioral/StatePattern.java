a1ed003ac9e4990a5417fc14ac8f9f2552df7dcb
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-03-30 21:42:10 +0530

    Removed comments

--- designpatterns/designpatterns/src/main/java/com/debasish/designpatterns/behavioral/StatePattern.java
+++ designpatterns/designpatterns/src/main/java/com/debasish/designpatterns/behavioral/StatePattern.java
@@ -2,11 +2,9 @@ package com.debasish.designpatterns.behavioral;
 
 public class StatePattern {
     public static void main(String[] args) {
-        // Step 1: Create State interface
         interface State {
-            void handle(); // behavior method
+            void handle();
         }
-        // Step 2: Create different states
         class RedState implements State {
             public void handle() {
                 System.out.println("RED Light -> Stop");
@@ -22,25 +20,22 @@ public class StatePattern {
                 System.out.println("YELLOW Light -> Wait");
             }
         }
-        // Step 3: Context class (Traffic Signal)
         class TrafficSignal {
-            State state; // current state
+            State state;
             void setState(State state) {
-                this.state = state; // change state
+                this.state = state;
             }
             void showSignal() {
-                state.handle(); // call behavior based on state
+                state.handle();
             }
         }
 
-        // Step 4: Use the State Pattern
-
-        TrafficSignal signal = new TrafficSignal(); // create context
-        signal.setState(new RedState()); // set RED
-        signal.showSignal(); // Stop
-        signal.setState(new GreenState()); // change to GREEN
-        signal.showSignal(); // Go
-        signal.setState(new YellowState()); // change to YELLOW
-        signal.showSignal(); // Wait
+        TrafficSignal signal = new TrafficSignal();
+        signal.setState(new RedState());
+        signal.showSignal();
+        signal.setState(new GreenState());
+        signal.showSignal();
+        signal.setState(new YellowState());
+        signal.showSignal();
     }
 }
