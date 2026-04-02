aead2ee9e7c0b9662378f68e74a7f5825743602c
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-02 21:09:57 +0530

    Comment Removed

--- designpatterns/designpatterns/src/main/java/com/debasish/designpatterns/creational/FactoryPattern.java
+++ designpatterns/designpatterns/src/main/java/com/debasish/designpatterns/creational/FactoryPattern.java
@@ -6,7 +6,6 @@ interface Shape {
 }
 class Circle implements Shape {
     private double radius;
-
     public Circle(double radius) {
         this.radius = radius;
     }
@@ -26,7 +25,6 @@ class Circle implements Shape {
 class Rectangle implements Shape {
     private double width;
     private double height;
-
     public Rectangle(double width, double height) {
         this.width = width;
         this.height = height;
